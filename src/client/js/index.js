49e7719d89125ec71cf3ff6952d8ac3b5cf64789
Steve Birtles <sbirtles@farnborough.ac.uk>
2018-04-25 11:15:04 +0100

    Fruits galore!

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -6,8 +6,8 @@ $(function() {
 
         let content = '';
         for (let fruit of data) {
-            content += '<span class="badge badge-primary p-2 m-2">' + fruit.name
-                + '<button class="delete_fruit btn btn-sm btn-danger ml-2"> x</button></span>';
+            content += '<span class="badge badge-primary p-2 m-2">' + fruit.name +
+                '<button class="delete_fruit btn btn-sm btn-danger ml-2"> x</button></span>';
         }
 
         $('#test-target').html(content);
